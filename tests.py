dc996a547c3a7503930a5906b451e701665082fe
Galina Baturina <g.bat.study@yandex.ru>
2024-09-19 01:06:19 +0700

    add all files from my project

--- tests.py
+++ tests.py
@@ -1,7 +1,5 @@
 import pytest
 
-from main import BooksCollector
-
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -12,21 +10,13 @@ class TestBooksCollector:
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self, book):
-        # создаем экземпляр (объект) класса BooksCollector
-        # collector = BooksCollector()
-
-        # добавляем две книги
         book.add_new_book('Гордость и предубеждение и зомби')
         book.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(book.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
     def test_add_new_book_book_added_just_once(self, book):
-        # collector = BooksCollector()
         book.add_new_book('Гордость и предубеждение')
         book.add_new_book('Гордость и предубеждение')
         assert len(book.get_books_genre()) == 1
@@ -40,7 +30,6 @@ class TestBooksCollector:
         ['Призрак дома на холме', 'Ужасы'], ['Дюна', 'Фантастика'], ['Трое в лодке, не считая собаки', 'Комедии']
     ])
     def test_get_book_genre_add_genre_for_book_true(self, book, name, genre):
-        # collector = BooksCollector()
         book.add_new_book(name)
         book.set_book_genre(name, genre)
         assert book.get_book_genre(name) == genre
